cb268de4e0b9802ef3d52355805ca55b6ed72d90
Lucky-Firstsnow <firstsnow1119@163.com>
2023-10-24 18:47:17 +0800

    1 day
    1 day

---
+++ kblog/src/main/java/com/lucky/kblog/KblogApplication.java
@@ -0,0 +1,25 @@
+package com.lucky.kblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KblogApplication.class, args);
+        System.out.println(
+                "(♥◠‿◠)ﾉﾞ  FisrstsnowLucky   ლ(´ڡ`ლ)ﾞ  \n" +
+                        " ..------------   |--|\n" +
+                        " ||------------   |--|\n" +
+                        " ||   | .. |      |--|\n" +
+                        " ||------------   |--|\n" +
+                        " ||------------   |--|\n" +
+                        " ||               |--|\n" +
+                        " ||               |--|\n" +
+                        " ||               |--|-------------|\n" +
+                        " ||               |--|-------------|\n"+
+                        "(♥◠‿◠)ﾉﾞ  FisrstsnowLucky   ლ(´ڡ`ლ)ﾞ");
+    }
+
+}
---
+++ kblog/src/test/java/com/lucky/kblog/KblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucky.kblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
